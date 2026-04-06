8646e5867d2068cc33c5388cb15bd27c4f560dc1
arsema16 <arsema2328@gmail.com>
2026-04-06 16:30:19 +0300

    up

--- portfolio/src/pages/Project/Modal.jsx
+++ portfolio/src/pages/Project/Modal.jsx
@@ -10,10 +10,10 @@ const Modal = ({
             {estado &&
                 <div className="overlay" onClick={() => cambiarEstado(false)}>
                     <div className='contenedor-modal' onClick={(e) => e.stopPropagation()}>
-                        <button className="boton-cerrar" onClick={() => cambiarEstado(false)}>
-                            ✕
-                        </button>
                         <div className="modal-scroll-content">
+                            <div className="modal-close-row">
+                                <button className="boton-cerrar" onClick={() => cambiarEstado(false)}>✕</button>
+                            </div>
                             {children}
                         </div>
                     </div>
